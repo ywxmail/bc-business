66624cc5444e9a64f5f5793cf8b135231eafcbfb
rongjihuang <rongjihuang@gmail.com>
2011-08-11 13:07:52 +0800

    增加OptionConstants常数类

---
+++ bc-business-base/src/main/java/cn/bc/business/OptionConstants.java
@@ -0,0 +1,24 @@
+package cn.bc.business;
+
+/**
+ * 营运子系统OptionGroup的Key常数定义
+ * 
+ * @author dragon
+ * 
+ */
+public class OptionConstants extends cn.bc.option.OptionConstants {
+	/** 车辆燃料类型 */
+	public final static String CAR_FUEL_TYPE = "car.fuel.type";
+
+	/** 车辆营运性质 */
+	public final static String CAR_BUSINESS_NATURE = "car.business.nature";
+
+	/** 车辆颜色 */
+	public final static String CAR_COLOR = "car.color";
+
+	/** 车辆定级 */
+	public final static String CAR_RANK = "car.rank";
+
+	/** 车辆厂牌 */
+	public final static String CAR_BRAND = "car.brand";
+}
