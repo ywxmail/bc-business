bb9b390dbcd17eab636ec331aa650505612cb809
rongjihuang <rongjihuang@gmail.com>
2011-09-16 16:16:56 +0800

    交通违章、营运违章、事故理赔

---
+++ bc-business-runcase/src/main/java/cn/bc/business/runcase/dao/hibernate/jpa/CaseBaseDaoImpl.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package cn.bc.business.runcase.dao.hibernate.jpa;
+
+import cn.bc.business.runcase.dao.CaseBaseDao;
+import cn.bc.business.runcase.domain.CaseBase;
+import cn.bc.orm.hibernate.jpa.HibernateCrudJpaDao;
+
+
+/**
+ * 营运事件Dao的hibernate jpa实现
+ * 
+ * @author dragon
+ */
+public class CaseBaseDaoImpl extends HibernateCrudJpaDao<CaseBase> implements CaseBaseDao{
+
+}
---
+++ bc-business-runcase/src/main/java/cn/bc/business/runcase/service/CaseBaseServiceImpl.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package cn.bc.business.runcase.service;
+
+import cn.bc.business.runcase.dao.CaseBaseDao;
+import cn.bc.business.runcase.domain.CaseBase;
+import cn.bc.core.service.DefaultCrudService;
+
+/**
+ * 营运事件Service的实现
+ * 
+ * @author dragon
+ */
+public class CaseBaseServiceImpl extends DefaultCrudService<CaseBase> implements
+		CaseBaseService {
+	private CaseBaseDao caseBaseDao;
+
+	public CaseBaseDao getCaseBaseDao() {
+		return caseBaseDao;
+	}
+
+	public void setCaseBaseDao(CaseBaseDao caseBaseDao) {
+		this.caseBaseDao = caseBaseDao;
+		this.setCrudDao(caseBaseDao);
+	}
+}
