ebdca0f6d4ce816f82ffff8d405fd0714026b6b1
rongjihuang <rongjihuang@gmail.com>
2011-10-12 19:17:50 +0800

    车辆选择车队范围的修改

--- bc-business-car/src/main/java/cn/bc/business/car/web/struts2/CarAction.java
+++ bc-business-car/src/main/java/cn/bc/business/car/web/struts2/CarAction.java
@@ -186,13 +186,12 @@ public class CarAction extends FileEntityAction<Long, Car> {
 		return columns;
 	}
 
-	@SuppressWarnings("static-access")
 	@Override
 	public String create() throws Exception {
 		String r = super.create();
 		this.getE().setUnit(this.getSystyemContext().getUnit());
 		this.getE().setUid(
-				this.getIdGeneratorService().next(this.getE().KEY_UID));
+				this.getIdGeneratorService().next(Car.KEY_UID));
 
 		// 初始化车辆的状态
 		this.getE().setStatus(RichEntityImpl.STATUS_DISABLED);
@@ -238,7 +237,7 @@ public class CarAction extends FileEntityAction<Long, Car> {
 	// 表单可选项的加载
 	public void initSelects() {
 		// 加载可选车队列表
-		this.motorcadeList = this.motorcadeService.createQuery().list();
+		this.motorcadeList = this.motorcadeService.findActive();
 		// 加载可选营运性质列表
 		this.businessTypeList = this.optionService
 				.findOptionItemByGroupKey(OptionConstants.CAR_BUSINESS_NATURE);
