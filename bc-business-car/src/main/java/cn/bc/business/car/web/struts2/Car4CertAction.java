f37b7362177f2c0c1cea72df05497e82a69d05bd
rongjihuang <rongjihuang@gmail.com>
2011-10-24 22:47:34 +0800

    车辆证件权限调整

--- bc-business-car/src/main/java/cn/bc/business/car/web/struts2/Car4CertAction.java
+++ bc-business-car/src/main/java/cn/bc/business/car/web/struts2/Car4CertAction.java
@@ -8,19 +8,18 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 
-import cn.bc.business.car.service.CarService;
 import cn.bc.business.cert.domain.Cert;
 import cn.bc.business.cert.web.struts2.CertAction;
 import cn.bc.core.Page;
 import cn.bc.core.exception.CoreException;
 import cn.bc.core.query.condition.impl.OrderCondition;
 import cn.bc.docs.web.ui.html.AttachWidget;
+import cn.bc.identity.web.SystemContext;
 import cn.bc.web.formater.AbstractFormater;
 import cn.bc.web.formater.CalendarFormater;
 import cn.bc.web.formater.CalendarRangeFormater;
@@ -48,23 +47,22 @@ import cn.bc.web.ui.json.Json;
 public class Car4CertAction extends CertAction {
 	// private static Log logger = LogFactory.getLog(BulletinAction.class);
 	private static final long serialVersionUID = 1L;
-	public 	boolean 			isManager;
-	public 	Long				carId;
-	public	CarService			carService;
+	public Long carId;
+	public Map<String, String> statusesValue;
 
-	public 	Map<String, String> statusesValue;
-
-	@Autowired
-	public void CarService(CarService carService) {
-		this.carService = carService;
+	@Override
+	public boolean isReadonly() {
+		// 车辆证件管理员或系统管理员
+		SystemContext context = (SystemContext) this.getContext();
+		return !context.hasAnyRole(getText("key.role.bs.cert4car"),
+				getText("key.role.bc.admin"));
 	}
-	
-	
+
 	@Override
 	public Class<? extends Cert> getEntityClass() {
 		return Cert.class;
 	}
-	
+
 	@Override
 	protected PageOption buildFormPageOption() {
 		PageOption option = new PageOption().setWidth(680).setMinWidth(250)
@@ -72,32 +70,32 @@ public class Car4CertAction extends CertAction {
 		if (!isReadonly()) {
 			option.addButton(new ButtonOption(getText("label.save"), "save"));
 		}
-		return option; 
+		return option;
 	}
 
 	public AttachWidget attachsUI;
 
-//	@SuppressWarnings("static-access")
+	// @SuppressWarnings("static-access")
 	@Override
 	public String create() throws Exception {
 		return "showdialog";
 	}
-	
+
 	// 删除
 	@Override
 	public String delete() throws Exception {
 		if (this.getId() != null) {// 删除一条
-			//单个删除中间表carman_cert表
+			// 单个删除中间表carman_cert表
 			this.certService.deleteCarNCert(this.getId());
-			//单个删除本表
+			// 单个删除本表
 			this.getCrudService().delete(this.getId());
 		} else {// 删除一批
 			if (this.getIds() != null && this.getIds().length() > 0) {
-				//批量删除中间表carman_cert表
+				// 批量删除中间表carman_cert表
 				Long[] certIds = cn.bc.core.util.StringUtils
 						.stringArray2LongArray(this.getIds().split(","));
 				this.certService.deleteCarNCert(certIds);
-				//批量删除本表
+				// 批量删除本表
 				Long[] ids = cn.bc.core.util.StringUtils
 						.stringArray2LongArray(this.getIds().split(","));
 				this.getCrudService().delete(ids);
@@ -108,32 +106,30 @@ public class Car4CertAction extends CertAction {
 		return "deleteSuccess";
 	}
 
-	
-
 	@SuppressWarnings("unused")
 	private AttachWidget buildAttachsUI(boolean isNew) {
-		isManager = isReadonly();
 		// 构建附件控件
-//		String ptype = "car.main";
-//		AttachWidget attachsUI = new AttachWidget();
-//		attachsUI.setFlashUpload(this.isFlashUpload());
-//		attachsUI.addClazz("formAttachs");
-//		if (!isNew)
-//			attachsUI.addAttach(this.attachService.findByPtype(ptype, this
-//					.getE().getUid()));
-//		attachsUI.setPuid(this.getE().getUid()).setPtype(ptype);
-//
-//		// 上传附件的限制
-//		attachsUI.addExtension(getText("app.attachs.extensions"))
-//				.setMaxCount(Integer.parseInt(getText("app.attachs.maxCount")))
-//				.setMaxSize(Integer.parseInt(getText("app.attachs.maxSize")));
-//		attachsUI.setReadOnly(!this.getE().isNew());
+		// String ptype = "car.main";
+		// AttachWidget attachsUI = new AttachWidget();
+		// attachsUI.setFlashUpload(this.isFlashUpload());
+		// attachsUI.addClazz("formAttachs");
+		// if (!isNew)
+		// attachsUI.addAttach(this.attachService.findByPtype(ptype, this
+		// .getE().getUid()));
+		// attachsUI.setPuid(this.getE().getUid()).setPtype(ptype);
+		//
+		// // 上传附件的限制
+		// attachsUI.addExtension(getText("app.attachs.extensions"))
+		// .setMaxCount(Integer.parseInt(getText("app.attachs.maxCount")))
+		// .setMaxSize(Integer.parseInt(getText("app.attachs.maxSize")));
+		// attachsUI.setReadOnly(!this.getE().isNew());
 		return attachsUI;
 	}
 
 	@Override
 	protected GridData buildGridData(List<Column> columns) {
-		return super.buildGridData(columns).setRowLabelExpression("['certCode']");
+		return super.buildGridData(columns).setRowLabelExpression(
+				"['certCode']");
 	}
 
 	@Override
@@ -146,7 +142,7 @@ public class Car4CertAction extends CertAction {
 	protected String getJs() {
 		return contextPath + "/bc-business/cert/car4cert/list.js";
 	}
-	
+
 	/** 构建视图页面的表格 */
 	protected Grid buildGrid() {
 		List<Column> columns = this.buildGridColumns();
@@ -169,24 +165,22 @@ public class Car4CertAction extends CertAction {
 
 		return grid;
 	}
-	
+
 	@Override
 	protected Toolbar buildToolbar() {
-		isManager = isReadonly();
 		Toolbar tb = new Toolbar();
-		
-		if (!isManager) {
+
+		if (!isReadonly()) {
 			// 新建按钮
 			tb.addButton(new ToolbarButton().setIcon("ui-icon-document")
 					.setText(getText("label.create"))
 					.setClick("bc.certcarList.create"));
-			
+
 			// 编辑按钮
 			tb.addButton(new ToolbarButton().setIcon("ui-icon-document")
 					.setText(getText("label.edit"))
 					.setClick("bc.certcarList.edit"));
 
-			
 			// 删除按钮
 			tb.addButton(getDefaultDeleteToolbarButton());
 		} else {// 普通用户
@@ -200,57 +194,58 @@ public class Car4CertAction extends CertAction {
 		return tb;
 	}
 
-	//搜索条件
+	// 搜索条件
 	@Override
 	protected String[] getSearchFields() {
-		return new String[] { "cert.certCode", "cert.certName", "car.plateType", "car.plateNo" };
+		return new String[] { "cert.certCode", "cert.certName",
+				"car.plateType", "car.plateNo" };
 	}
 
 	@Override
 	@SuppressWarnings("rawtypes")
 	protected List<Column> buildGridColumns() {
-		// 是否本模块管理员
-		isManager = isReadonly();
-
-		//List<Column> columns = super.buildGridColumns();
+		// List<Column> columns = super.buildGridColumns();
 		List<Column> columns = new ArrayList<Column>();
-		columns.add(new TextColumn("['id']", "ID",20));
-		columns.add(new TextColumn("['status']",getText("cert.status"),		50)
-				.setSortable(true).setValueFormater(new EntityStatusFormater(getEntityStatuses())));
-		columns.add(new TextColumn("['plateType']", getText("car")	)
-				.setSortable(true).setValueFormater(new AbstractFormater<String>() {
-					@Override
-					public String format(Object context, Object value) {
-						Map carName = (Map) context;
-						if(carName.get("plateType") != null){
-							return carName.get("plateType")+""+carName.get("plateNo")+""; 
-						}else{
-							return "";
-						}
-					}
-				}));
-		columns.add(new TextColumn("['certCode']",	getText("cert.certCode"),120)
-				.setSortable(true));
-		columns.add(new TextColumn("['type']", getText("cert.type"),80)
+		columns.add(new TextColumn("['id']", "ID", 20));
+		columns.add(new TextColumn("['status']", getText("cert.status"), 50)
+				.setSortable(true).setValueFormater(
+						new EntityStatusFormater(getEntityStatuses())));
+		columns.add(new TextColumn("['plateType']", getText("car"))
+				.setSortable(true).setValueFormater(
+						new AbstractFormater<String>() {
+							@Override
+							public String format(Object context, Object value) {
+								Map carName = (Map) context;
+								if (carName.get("plateType") != null) {
+									return carName.get("plateType") + ""
+											+ carName.get("plateNo") + "";
+								} else {
+									return "";
+								}
+							}
+						}));
+		columns.add(new TextColumn("['certCode']", getText("cert.certCode"),
+				120).setSortable(true));
+		columns.add(new TextColumn("['type']", getText("cert.type"), 80)
 				.setSortable(true).setUseTitleFromLabel(true)
 				.setValueFormater(new KeyValueFormater(getEntityTypes())));
-		columns.add(new TextColumn("['issueDate']", getText("cert.issueDate"),100)
-				.setSortable(true).setValueFormater(new CalendarFormater("yyyy-MM-dd")));
-		columns.add(new TextColumn("['startDate']", getText("cert.effectiveDate"),180)
-				.setSortable(true).setValueFormater(new CalendarRangeFormater("yyyy-MM-dd") {
+		columns.add(new TextColumn("['issueDate']", getText("cert.issueDate"),
+				100).setSortable(true).setValueFormater(
+				new CalendarFormater("yyyy-MM-dd")));
+		columns.add(new TextColumn("['startDate']",
+				getText("cert.effectiveDate"), 180).setSortable(true)
+				.setValueFormater(new CalendarRangeFormater("yyyy-MM-dd") {
 					@Override
-					public Calendar getToDate(Object context,Object value) {
+					public Calendar getToDate(Object context, Object value) {
 						Map date = (Map) context;
 						return (Calendar) date.get("endDate");
 					}
 				}));
-		columns.add(new TextColumn("['licencer']",	getText("cert.licencer"),100)
-				.setSortable(true));
+		columns.add(new TextColumn("['licencer']", getText("cert.licencer"),
+				100).setSortable(true));
 		return columns;
 	}
-	
 
-	
 	/**
 	 * 根据请求的条件查找非分页信息对象
 	 * 
@@ -258,27 +253,24 @@ public class Car4CertAction extends CertAction {
 	 */
 	@Override
 	protected List<? extends Object> findList() {
-		return this.certService.list4car(this.getCondition(),carId);
+		return this.certService.list4car(this.getCondition(), carId);
 	}
-	
-	
+
 	/**
 	 * 根据请求的条件查找分页信息对象
 	 * 
 	 * @return
 	 */
 	protected Page<? extends Object> findPage() {
-		return this.certService.page4car(
-					this.getCondition(),this.getPage().getPageNo(), 
-					this.getPage().getPageSize());
+		return this.certService.page4car(this.getCondition(), this.getPage()
+				.getPageNo(), this.getPage().getPageSize());
 	}
-	
-	//复写搜索URL方法
+
+	// 复写搜索URL方法
 	protected String getEntityConfigName() {
 		return "car4cert";
 	}
-	
-	
+
 	@Override
 	protected HtmlPage buildHtml4Paging() {
 		HtmlPage page = super.buildHtml4Paging();
@@ -287,6 +279,5 @@ public class Car4CertAction extends CertAction {
 					.toString());
 		return page;
 	}
-	
-	
+
 }
