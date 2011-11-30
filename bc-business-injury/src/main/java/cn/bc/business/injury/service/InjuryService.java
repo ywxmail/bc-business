63b0fc7888eefce7d99b10512af2d17b488d1659
bryanthe <bryanthe@163.com>
2011-11-30 16:30:38 +0800

    工伤模块初建

---
+++ bc-business-injury/src/main/java/cn/bc/business/injury/service/InjuryService.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package cn.bc.business.injury.service;
+
+import cn.bc.business.injury.domain.Injury;
+import cn.bc.core.service.CrudService;
+
+
+/**
+ * 工伤Service
+ * 
+ * @author wis
+ */
+public interface InjuryService extends CrudService<Injury> {
+
+}
