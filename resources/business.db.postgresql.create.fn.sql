db911001196c96dba7d364bc0a985cddc9b26e8d
rongjihuang <rongjihuang@gmail.com>
2011-12-21 15:08:19 +0800

    postgresql存储函数:获取指定车辆实时的营运司机信息

---
+++ resources/business.db.postgresql.create.fn.sql
@@ -0,0 +1,21 @@
+﻿-- ##营运子系统的 postgresql 自定义函数和存储过程##
+
+-- 获取指定车辆实时的营运司机信息
+-- 返回值的格式为：张三(正班),李四(副班),小明(顶班)
+-- 返回值是先按营运班次正序排序再按司机的入职时间正序排序进行合并的
+-- 参数：cid - 车辆的id
+CREATE OR REPLACE FUNCTION concatDriverInfoByCarId(cid IN integer) RETURNS varchar AS $$
+DECLARE
+	--定义变量
+	driverInfo varchar(4000);
+BEGIN
+	select string_agg(concat(name,'(',(case when classes=1 then '正班' when classes=2 then '副班' when classes=3 then '顶班' else '无' end),')'),',')
+		into driverInfo
+		from (select m.name as name,cm.classes as classes 
+			from BS_CAR_DRIVER cm
+			inner join BS_CARMAN m on m.id=cm.driver_id
+			where cm.status_=0 and cm.car_id=cid
+			order by cm.classes asc,m.work_date asc) as t;
+	return driverInfo;
+END;
+$$ LANGUAGE plpgsql;
