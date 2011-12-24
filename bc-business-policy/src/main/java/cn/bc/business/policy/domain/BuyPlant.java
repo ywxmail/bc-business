080ed309b8a59f8ac9847b4234980dab8a58ebab
zxr <991316914@qq.com>
2011-12-24 21:34:36 +0800

    添加购买的险种domain

---
+++ bc-business-policy/src/main/java/cn/bc/business/policy/domain/BuyPlant.java
@@ -0,0 +1,81 @@
+package cn.bc.business.policy.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import cn.bc.core.EntityImpl;
+
+/**
+ * 购买车保险种
+ * 
+ * @author dragon
+ */
+@Entity
+@Table(name = "BS_BUY_PLANT")
+public class BuyPlant extends EntityImpl {
+	private static final long serialVersionUID = 1L;
+
+	private String name;// 险种名称
+	private Float coverage;// 保额
+	private Float premium;// 保费
+	private String description;// 备注
+	private int orderNo;// 排序号
+	private Policy policy;// 车辆保单ID
+
+	@Column(name = "ORDER_")
+	public int getOrderNo() {
+		return orderNo;
+	}
+
+	public void setOrderNo(int orderNo) {
+		this.orderNo = orderNo;
+	}
+
+	@ManyToOne(fetch = FetchType.EAGER, optional = false)
+	@JoinColumn(name = "PID", referencedColumnName = "ID")
+	public Policy getPolicy() {
+		return policy;
+	}
+
+	public void setPolicy(Policy policy) {
+		this.policy = policy;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Float getCoverage() {
+		return coverage;
+	}
+
+	public void setCoverage(Float coverage) {
+		this.coverage = coverage;
+	}
+
+	public Float getPremium() {
+		return premium;
+	}
+
+	public void setPremium(Float premium) {
+		this.premium = premium;
+	}
+
+	@Column(name = "DESC_")
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
