f0ff8f0da06202afa7b35ccbe682fde9e9f3cea8
fatfish <fatfish2010@sina.cn>
2012-02-07 11:54:43 +0800

    险种视图增加类型、所属模板等列

--- bc-business-insuranceType/src/main/java/cn/bc/business/insuranceType/web/struts2/InsuranceTypesAction.java
+++ bc-business-insuranceType/src/main/java/cn/bc/business/insuranceType/web/struts2/InsuranceTypesAction.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
+import cn.bc.business.insuranceType.domain.InsuranceType;
 import cn.bc.business.web.struts2.ViewAction;
 import cn.bc.core.query.condition.Condition;
 import cn.bc.core.query.condition.ConditionUtils;
@@ -51,8 +52,8 @@ public class InsuranceTypesAction extends ViewAction<Map<String, Object>> {
 
 	@Override
 	protected OrderCondition getGridDefaultOrderCondition() {
-		// 默认排序方向：状态|创建日期
-		return new OrderCondition("i.status_", Direction.Asc).add(
+		// 默认排序方向：状态|类型|创建日期
+		return new OrderCondition("i.status_", Direction.Asc).add("i.type_", Direction.Desc).add(
 				"i.file_date", Direction.Desc);
 	}
 
@@ -62,8 +63,10 @@ public class InsuranceTypesAction extends ViewAction<Map<String, Object>> {
 
 		// 构建查询语句,where和order by不要包含在sql中(要统一放到condition中)
 		StringBuffer sql = new StringBuffer();
-		sql.append("select i.id,i.status_ ,i.name,i.coverage,i.desc_,i.file_date");
+		sql.append("select i.id as id,i.status_ as status,i.type_ as type,i.name as name ");
+		sql.append(" ,i.coverage as coverage,n.name as pname,i.desc_ as desc,i.file_date");
 		sql.append(" from bs_insurance_type i");
+		sql.append(" left join bs_insurance_type n on i.pid=n.id");
 		sqlObject.setSql(sql.toString());
 
 		// 注入参数
@@ -75,10 +78,12 @@ public class InsuranceTypesAction extends ViewAction<Map<String, Object>> {
 				Map<String, Object> map = new HashMap<String, Object>();
 				int i = 0;
 				map.put("id", rs[i++]);
-				map.put("status_", rs[i++]);
+				map.put("status", rs[i++]);
+				map.put("type", rs[i++]);
 				map.put("name", rs[i++]);
 				map.put("coverage", rs[i++]);
-				map.put("desc_", rs[i++]);
+				map.put("pname", rs[i++]);
+				map.put("desc", rs[i++]);
 				return map;
 			}
 		});
@@ -89,19 +94,36 @@ public class InsuranceTypesAction extends ViewAction<Map<String, Object>> {
 	protected List<Column> getGridColumns() {
 		List<Column> columns = new ArrayList<Column>();
 		columns.add(new IdColumn4MapKey("i.id", "id"));
-		columns.add(new TextColumn4MapKey("i.status_", "status_",
+		columns.add(new TextColumn4MapKey("i.status_", "status",
 				getText("label.status"), 60)
 				.setSortable(true)
 				.setValueFormater(new EntityStatusFormater(getEntityStatuses())));
+		//类型
+		columns.add(new TextColumn4MapKey("i.type_", "type",
+				getText("insuranceType.type"), 40)
+				.setSortable(true)
+				.setValueFormater(new EntityStatusFormater(this.getTypes())));
 		columns.add(new TextColumn4MapKey("i.name", "name",
-				getText("insuranceType.name"), 85).setSortable(true)
+				getText("insuranceType.name"), 120).setSortable(true)
 				.setUseTitleFromLabel(true));
 		columns.add(new TextColumn4MapKey("i.coverage", "coverage",
 				getText("insuranceType.coverage"), 80).setSortable(true)
 				.setUseTitleFromLabel(true));
-		columns.add(new TextColumn4MapKey("i.desc_", "desc_",
+		//所属模板名称
+		columns.add(new TextColumn4MapKey("n.name","pname",
+				getText("insuranceType.pname"),120).setSortable(true)
+				.setUseTitleFromLabel(true));
+		columns.add(new TextColumn4MapKey("i.desc_", "desc",
 				getText("insuranceType.description"),100).setSortable(true));
 		return columns;
+	}
+	
+	//类型键值转换
+	private Map<String,String> getTypes(){
+		Map<String,String> mtypes=new HashMap<String, String>();
+		mtypes.put(String.valueOf(InsuranceType.TYPE_PLANT), getText("insuranceType.type.plant"));
+		mtypes.put(String.valueOf(InsuranceType.TYPE_TEMPLATE), getText("insuranceType.type.template"));
+		return mtypes;
 	}
 
 	@Override
