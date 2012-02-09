66d03fee6e7a0ad5a6123094311a7aceb5d8d58f
zxr <991316914@qq.com>
2012-02-09 13:55:44 +0800

    插入迁移类型选项分组信息

--- bc-business-base/src/main/java/cn/bc/business/OptionConstants.java
+++ bc-business-base/src/main/java/cn/bc/business/OptionConstants.java
@@ -21,15 +21,13 @@ public class OptionConstants extends cn.bc.option.OptionConstants {
 
 	/** 车辆厂牌 */
 	public final static String CAR_BRAND = "car.brand";
-	
+
 	/** 所属公司 */
 	public final static String CAR_COMPANY = "car.company";
-	
+
 	/** 注销原因 */
 	public final static String CAR_LOGOUT_REASON = "car.logout.reason";
-	
-	
-	
+
 	/** 司机营运班次 */
 	public final static String DRIVER_CLASSES = "driver.classes";
 	/** 司机责任人区域 */
@@ -40,53 +38,51 @@ public class OptionConstants extends cn.bc.option.OptionConstants {
 	public final static String CARMAN_LEVEL = "carMan.level";
 	/** 司机责任人准驾车型 */
 	public final static String CARMAN_MODEL = "carMan.model";
-	/** 黑名单限制项目*/
+	/** 黑名单限制项目 */
 	public final static String BLACKLIST_TYPE = "blacklist.type";
-	
-	/** 计价器制造厂  */
+
+	/** 计价器制造厂 */
 	public final static String CAR_TAXIMETERFACTORY = "car.taximeterFactory";
-	
-	/** 签约类型	*/
+
+	/** 签约类型 */
 	public final static String CONTRACT_SIGNTYPE = "contract.signType";
-	
-	/** 合同版本号	*/
+
+	/** 合同版本号 */
 	public final static String CONTRACT_VERSION_NO = "contract.version.no";
-	
-	
-	/** 营运事件交通违章责任	*/
+
+	/** 营运事件交通违章责任 */
 	public final static String IT_DUTY = "it.duty";
-	/** 营运事件交通违章程度	*/
+	/** 营运事件交通违章程度 */
 	public final static String IT_DEGREE = "it.degree";
-	/** 营运事件交通违章性质	*/
+	/** 营运事件交通违章性质 */
 	public final static String IT_PROPERITES = "it.properties";
-	/** 营运事件营运没收证件	*/
+	/** 营运事件营运没收证件 */
 	public final static String BS_CERT = "bs.cert";
-	/** 营运事件事故理赔性质	*/
+	/** 营运事件事故理赔性质 */
 	public final static String BS_PROPERITES = "bs.properties";
-	/** 营运事件事故理赔司机类型	*/
+	/** 营运事件事故理赔司机类型 */
 	public final static String CA_CARMANTYPE = "ca.carmantype";
-	/** 营运事件事故理赔处理部门	*/
+	/** 营运事件事故理赔处理部门 */
 	public final static String CA_DEPARTMENT = "ca.department";
-	/** 营运事件事故理赔保险公司	*/
+	/** 营运事件事故理赔保险公司 */
 	public final static String CA_COMPANY = "ca.company";
-	/** 营运事件事故理赔事故性质	*/
+	/** 营运事件事故理赔事故性质 */
 	public final static String CA_SORT = "ca.sort";
-	/** 营运事件事故理赔责任	*/
+	/** 营运事件事故理赔责任 */
 	public final static String CA_DUTY = "ca.duty";
-	/** 投诉建议来源	*/
+	/** 投诉建议来源 */
 	public final static String AD_SOURCE = "ad.source";
-	/** 表扬类型	*/
+	/** 表扬类型 */
 	public final static String PR_TYPE = "pr.type";
-	
-	
-	/** 社保险种	*/
+
+	/** 社保险种 */
 	public final static String LB_INSURANCETYPE = "lb.insuranceType";
-	/** 购买单位	*/
+	/** 购买单位 */
 	public final static String LB_BUYUNIT = "lb.buyUnit";
-	
-	
-	
-	
+
+	/** 迁移类型 */
+	public final static String CARBYDRIVERHISTORY_MOVETYPE = "carByDriverHistory.moveType";
+
 	/** 车队的可选缴费日 */
 	public final static String MOTORCADE_PAYMENT_DATE = "motorcade.paymentDate";
 }
