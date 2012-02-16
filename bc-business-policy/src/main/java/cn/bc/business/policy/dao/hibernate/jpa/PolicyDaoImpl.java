5f7e46039438cb586ebbc20df62a7406c2d823d1
fatfish <fatfish2010@sina.cn>
2012-02-16 14:28:20 +0800

    车辆保单增加通过时间和车辆获取保单信息的接口

--- bc-business-policy/src/main/java/cn/bc/business/policy/dao/hibernate/jpa/PolicyDaoImpl.java
+++ bc-business-policy/src/main/java/cn/bc/business/policy/dao/hibernate/jpa/PolicyDaoImpl.java
@@ -3,16 +3,35 @@
  */
 package cn.bc.business.policy.dao.hibernate.jpa;
 
+import java.util.Calendar;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import cn.bc.business.policy.dao.PolicyDao;
 import cn.bc.business.policy.domain.Policy;
 import cn.bc.orm.hibernate.jpa.HibernateCrudJpaDao;
-
 
 /**
  * 车辆保单Dao的hibernate jpa实现
  * 
  * @author dragon
  */
-public class PolicyDaoImpl extends HibernateCrudJpaDao<Policy> implements PolicyDao{
+public class PolicyDaoImpl extends HibernateCrudJpaDao<Policy> implements
+		PolicyDao {
+
+	private static Log logger = LogFactory.getLog(PolicyDaoImpl.class);
+
+	@SuppressWarnings("unchecked")
+	public List<Policy> getPolicise(Long carId, Calendar happenTime) {
+		String hql = "from Policy  where car.id=? and";
+		hql += " ((? >= commerialStartDate and ? <= commerialEndDate) or (? >= greenslipStartDate and ? <= greenslipEndDate))";
+		if (logger.isDebugEnabled()) {
+			logger.debug("hql=" + hql);
+		}
+		return this.getJpaTemplate().find(hql, carId, happenTime, happenTime,
+				happenTime, happenTime);
+	}
 
 }
