fc2b5e59915edcc44f3eac68b4768bd79b07813a
bryanthe <bryanthe@163.com>
2012-02-16 09:00:49 +0800

    车型配置模块initForm函数增加异常处理

--- bc-business-car/src/main/java/cn/bc/business/car/web/struts2/CarAction.java
+++ bc-business-car/src/main/java/cn/bc/business/car/web/struts2/CarAction.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
