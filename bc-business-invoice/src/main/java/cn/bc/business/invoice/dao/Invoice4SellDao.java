c0a5f7b4e76a87339ad5b62b4efcf8a30e476652
bryanthe <bryanthe@163.com>
2012-03-19 12:02:59 +0800

    1.指定日期前 总采购数 (传入参数：发票类型，公司，日期） 2.指定日期前 总销售数	(传入参数：发票类型，公司，日期） 3.指定日期区间 采购数 (传入参数：发票类型，公司，日期1,日期2） 4.指定日期区间 销售数	(传入参数：发票类型，公司，日期1,日期2）

--- bc-business-invoice/src/main/java/cn/bc/business/invoice/dao/Invoice4SellDao.java
+++ bc-business-invoice/src/main/java/cn/bc/business/invoice/dao/Invoice4SellDao.java
@@ -3,6 +3,7 @@
  */
 package cn.bc.business.invoice.dao;
 
+import java.util.Calendar;
 import java.util.List;
 
 import cn.bc.business.invoice.domain.Invoice4Sell;
@@ -30,5 +31,45 @@ public interface Invoice4SellDao extends CrudDao<Invoice4Sell> {
 	 * @return
 	 */
 	List<Invoice4SellDetail> selectSellDetailByCode(Long buyId, Long sellId);
+	
+	
+	/**
+	 * 指定采购日期 总采购数 (company为null即查宝城和广发所有记录)
+	 * @param type
+	 * @param buyDate
+	 * @param company
+	 * @return
+	 */
+	int countInvoiceBuyCountByBuyDate(Integer type,Calendar buyDate,String company);
+	
+	/**
+	 * 指定采购日期范围 总采购数(company为null即查宝城和广发所有记录)
+	 * @param type
+	 * @param buyDateFrom
+	 * @param buyDateTo
+	 * @param company
+	 * @return
+	 */
+	int countInvoiceBuyCountByBuyDate(Integer type,Calendar buyDateFrom, Calendar buyDateTo, String company);
+	
+	/**
+	 * 指定销售日期 总销售数(company为null即查宝城和广发所有记录)
+	 * @param type
+	 * @param SellDate
+	 * @param company
+	 * @return
+	 */
+	int countInvoiceSellCountBySellDate(Integer type,Calendar SellDate, String company);
+	
+	/**
+	 * 指定销售日期范围  总销售数(company为null即查宝城和广发所有记录)
+	 * @param type
+	 * @param SellDateFrom
+	 * @param SellDateTo
+	 * @param company
+	 * @return
+	 */
+	int countInvoiceSellCountBySellDate(Integer type,Calendar SellDateFrom, Calendar SellDateTo, String company);
+	
 
 }
