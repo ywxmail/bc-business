62fceff1d916bf0ddefe454139e01cb1d84496b6
fatfish <fatfish2010@sina.cn>
2012-03-19 18:49:24 +0800

    发票余额表Action增加访问入口和AJAX请求查询结果的方法

--- bc-business-invoice/src/main/java/cn/bc/business/invoice/web/struts2/Invoice4BalanceAction.java
+++ bc-business-invoice/src/main/java/cn/bc/business/invoice/web/struts2/Invoice4BalanceAction.java
@@ -3,6 +3,10 @@
  */
 package cn.bc.business.invoice.web.struts2;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,8 +17,12 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
+import cn.bc.business.invoice.domain.Invoice4Buy;
 import cn.bc.business.invoice.service.Invoice4BuyService;
 import cn.bc.business.invoice.service.Invoice4SellService;
+import cn.bc.web.ui.html.page.ButtonOption;
+import cn.bc.web.ui.html.page.PageOption;
+import cn.bc.web.ui.json.Json;
 
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -33,10 +41,10 @@ public class Invoice4BalanceAction extends ActionSupport {
 	private static final long serialVersionUID = 1L;
 	private Invoice4SellService invoice4SellService;
 	private Invoice4BuyService invoice4BuyService;
-
-	
+	public PageOption pageOption;
 
 	public List<Map<String, String>> typeList; // 发票类型列表（打印票、手撕票）
+	public List<Map<String, String>> companyList; // 公司
 
 	@Autowired
 	public void setInvoice4BuyService(Invoice4SellService invoice4SellService) {
@@ -48,5 +56,86 @@ public class Invoice4BalanceAction extends ActionSupport {
 		this.invoice4BuyService = invoice4BuyService;
 	}
 
-	
+	public Calendar startDate;
+	public Calendar endDate;
+
+	public String startCount;// 期初数量
+	public String endCount;// 收入数量
+
+	@Override
+	public String execute() throws Exception {
+		// 设置页面
+		this.pageOption = new PageOption()
+				.setMaximizable(true)
+				.setMinimizable(true)
+				.setMinWidth(200)
+				.setWidth(310)
+				.setMinHeight(200)
+				.setHeight(300)
+				.addButton(
+						new ButtonOption(getText("invoice.optype.select"),
+								null, "bs.invoice4BalanceForm.select"));
+
+		// 设置日期
+		this.startDate = Calendar.getInstance();
+		this.endDate = Calendar.getInstance();
+
+		// 发票类型
+		List<Map<String, String>> list = new ArrayList<Map<String, String>>();
+		list.add(this.getOptiomItems(String.valueOf(Invoice4Buy.TYPE_PRINT),
+				getText("invoice.type.dayinpiao")));
+		list.add(this.getOptiomItems(String.valueOf(Invoice4Buy.TYPE_TORE),
+				getText("invoice.type.shousipiao")));
+		this.typeList = list;
+
+		// 公司
+		this.companyList = this.invoice4BuyService.findCompany4Option();
+
+		int count = this.invoice4SellService.countInvoiceBuyCountByBuyDate(
+				Invoice4Buy.TYPE_PRINT, Calendar.getInstance(), null)
+				- this.invoice4SellService.countInvoiceSellCountBySellDate(
+						Invoice4Buy.TYPE_PRINT, Calendar.getInstance(), null);
+		DecimalFormat format = new DecimalFormat("###,###.##");
+		this.startCount = format.format(count);
+		this.endCount = format.format(count);
+		return super.execute();
+	}
+
+	/**
+	 * 生成OptiomItem key、value值
+	 */
+	private Map<String, String> getOptiomItems(String key, String value) {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("key", key);
+		map.put("value", value);
+		return map;
+	}
+
+	public String json;// ajax返回的json信息
+	public Integer type;// 发票类型
+	public String company;// 公司
+
+	public String select() {
+		Json json = new Json();
+		json.put("startCount", String.valueOf(this.invoice4SellService
+				.countInvoiceBuyCountByBuyDate(this.type, this.startDate,
+						this.company)
+				- this.invoice4SellService.countInvoiceSellCountBySellDate(
+						this.type, this.startDate, this.company)));
+		json.put("buyCount", String.valueOf(this.invoice4SellService
+				.countInvoiceBuyCountByBuyDate(this.type, this.startDate,
+						this.endDate, this.company)));
+		json.put("sellCount", String.valueOf(this.invoice4SellService
+				.countInvoiceSellCountBySellDate(this.type, this.startDate,
+						this.endDate, this.company)));
+		json.put("endCount", String.valueOf(this.invoice4SellService
+				.countInvoiceBuyCountByBuyDate(this.type, this.endDate,
+						this.company)
+				- this.invoice4SellService.countInvoiceSellCountBySellDate(
+						this.type, this.endDate, this.company)));
+
+		this.json = json.toString();
+		return "json";
+	}
+
 }
