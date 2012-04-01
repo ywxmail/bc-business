6511881a1759e3b7a38c95af851147f9d2b041e2
zxr <991316914@qq.com>
2012-04-01 11:41:36 +0800

    修正黑名单批量删除时出错

--- bc-business-blacklist/src/main/java/cn/bc/business/blacklist/service/BlacklistServiceImpl.java
+++ bc-business-blacklist/src/main/java/cn/bc/business/blacklist/service/BlacklistServiceImpl.java
@@ -3,6 +3,8 @@
  */
 package cn.bc.business.blacklist.service;
 
+import java.io.Serializable;
+
 import cn.bc.business.blacklist.dao.BlacklistDao;
 import cn.bc.business.blacklist.domain.Blacklist;
 import cn.bc.core.service.DefaultCrudService;
@@ -23,6 +25,21 @@ public class BlacklistServiceImpl extends DefaultCrudService<Blacklist>
 	public void setBlacklistDao(BlacklistDao blacklistDao) {
 		this.blacklistDao = blacklistDao;
 		this.setCrudDao(blacklistDao);
+	}
+
+	@Override
+	public void delete(Serializable id) {
+		// TODO Auto-generated method stub
+		super.delete(id);
+	}
+
+	@Override
+	public void delete(Serializable[] ids) {
+		// 循环删除
+		for (Serializable id : ids) {
+			super.delete(id);
+		}
+
 	}
 
 }
