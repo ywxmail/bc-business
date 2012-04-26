8a8e3a9d9a08f9d36102f8ebdbf47b87e7106f60
zxr <991316914@qq.com>
2012-04-26 17:47:44 +0800

    修正页签下单选按钮选全部时返回数据全部数据的错误

--- bc-business-carman/src/main/java/cn/bc/business/carman/web/struts2/CarByDriverHistorysAction.java
+++ bc-business-carman/src/main/java/cn/bc/business/carman/web/struts2/CarByDriverHistorysAction.java
@@ -303,12 +303,11 @@ public class CarByDriverHistorysAction extends ViewAction<Map<String, Object>> {
 				statusCondition = new InCondition("m.status_",
 						StringUtils.stringArray2IntegerArray(ss));
 			}
-		} else {
-			return null;
 		}
 
 		// carManId条件
 		Condition carManIdCondition = null;
+
 		if (carManId != null) {
 			carManIdCondition = new EqualsCondition("d.driver_id", carManId);
 		}
