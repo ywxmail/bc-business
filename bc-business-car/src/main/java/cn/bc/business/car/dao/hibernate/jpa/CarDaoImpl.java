82b0a6641f0530f5ff680cd804d8e00e138e5564
fatfish <fatfish2010@sina.cn>
2012-08-16 08:30:25 +0800

    获取交车信息查询sql修改

--- bc-business-car/src/main/java/cn/bc/business/car/dao/hibernate/jpa/CarDaoImpl.java
+++ bc-business-car/src/main/java/cn/bc/business/car/dao/hibernate/jpa/CarDaoImpl.java
@@ -562,7 +562,7 @@ public class CarDaoImpl extends HibernateCrudJpaDao<Car> implements CarDao {
 		sql+=",f.name as unitCompany,f.id as unitCompanyId,to_char(c.end_date,'YYYY-MM-DD') as ccEndDate";
 		sql+=",to_char(d.commerial_end_date,'YYYY-MM-DD') as commerialEndDate,to_char(d.greenslip_end_date,'YYYY-MM-DD') as greenslipEndDate";
 		sql+=",a.factory_type,to_char(d.greenslip_Start_Date,'YYYY-MM-DD') as greenslipStartDate,a.engine_no,a.vin,a.access_count,a.access_weight,a.displacement";
-		sql+=",(select string_agg(oman.name,',') from (select man.name form bs_carman man inner join bs_car_driver cd on man.id = cd.driver_id"; 
+		sql+=",(select string_agg(oman.name,',') from (select man.name from bs_carman man inner join bs_car_driver cd on man.id = cd.driver_id"; 
 		sql+=" where cd.car_id = a.id and cd.status_ = 0 ORDER BY man.id) oman) as driver";
 		sql+=",(select string_agg(oman.cert_fwzg,',') from (select man.cert_fwzg from bs_carman man inner join bs_car_driver cd on man.id = cd.driver_id"; 
 		sql+=" where cd.car_id = a.id and cd.status_ = 0 ORDER BY man.id) oman) as driverFWZG";
